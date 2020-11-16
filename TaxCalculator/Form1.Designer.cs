fce24e2dc2fd2e62bb980906d0c3911c7a5a1f22
Josiah134 <74517148+Josiah134@users.noreply.github.com>
2020-11-15 19:45:21 -0600

    Add files via upload

---
+++ TaxCalculator/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace TaxCalculator
+{
+	partial class TaxCalculator
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(13, 22);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "Input";
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Alabama",
+            "Alaska",
+            "Arizona",
+            "Arkansas",
+            "California",
+            "Colorado",
+            "Connecticut",
+            "Delaware",
+            "Florida",
+            "Georgia",
+            "Hawaii",
+            "Idaho",
+            "Illinois",
+            "Indiana",
+            "Iowa",
+            "Kansas",
+            "Kentucky",
+            "Louisiana",
+            "Maine",
+            "Maryland",
+            "Massachusetts",
+            "Michigan",
+            "Minnesota",
+            "Mississippi",
+            "Missouri",
+            "MontanaNebraska",
+            "Nevada",
+            "New Hampshire",
+            "New Jersey",
+            "New Mexico",
+            "New York",
+            "North Carolina",
+            "North Dakota",
+            "Ohio",
+            "Oklahoma",
+            "Oregon",
+            "Pennsylvania",
+            "Rhode Island",
+            "South Carolina",
+            "South Dakota",
+            "Tennessee",
+            "Texas",
+            "Utah",
+            "Vermont",
+            "Virginia",
+            "Washington",
+            "West Virginia",
+            "Wisconsin",
+            "Wyoming"});
+			this.comboBox1.Location = new System.Drawing.Point(119, 21);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 1;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 100);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Calculate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(13, 61);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 3;
+			this.textBox2.Text = "Output";
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(119, 48);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(93, 17);
+			this.checkBox1.TabIndex = 4;
+			this.checkBox1.Text = "Grab Location";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// TaxCalculator
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(267, 138);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "TaxCalculator";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+	}
+}
+
---
+++ TaxCalculator/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaxCalculator
+{
+	public partial class TaxCalculator : Form
+	{
+		public TaxCalculator()
+		{
+			InitializeComponent();
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if(checkBox1.Checked)
+			{
+				string externalip = new WebClient().DownloadString("http://icanhazip.com");
+				var web = new WebClient();
+				var url = $"http://ip-api.com/line/" + externalip;
+				var responseString = web.DownloadString(url);
+				String[] rsplit = responseString.Split('\n');
+				comboBox1.SelectedItem = rsplit[4];
+				string state = comboBox1.SelectedItem.ToString();
+				int counter = 0;
+				string line;
+
+				System.IO.StreamReader file = new System.IO.StreamReader(@"Taxes.txt");
+				while ((line = file.ReadLine()) != null)
+				{
+					String[] split = line.Split(':');
+					if (split[0] == state)
+					{
+						decimal output = decimal.Parse(textBox1.Text) * decimal.Parse(split[1]) / 100;
+						textBox2.Text = output.ToString();
+						break;
+					}
+					counter++;
+				}
+
+			}
+			else
+			{
+				string state = comboBox1.SelectedItem.ToString();
+				int counter = 0;
+				string line;
+
+				System.IO.StreamReader file = new System.IO.StreamReader(@"Taxes.txt");
+				while ((line = file.ReadLine()) != null)
+				{
+					String[] split = line.Split(':');
+					if (split[0] == state)
+					{
+						decimal output = decimal.Parse(textBox1.Text) * decimal.Parse(split[1]) / 100;
+						textBox2.Text = output.ToString();
+						break;
+					}
+					counter++;
+				}
+			}
+		}
+	}
+}
